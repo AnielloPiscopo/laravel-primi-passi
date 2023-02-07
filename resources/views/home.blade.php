fd48c0b93c6a1d25748ccf278ae80328dcec6f17
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-02-07 17:11:16 +0100

    Added the content in the 2 other pages blade and improved the navbar system

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,9 +22,9 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                   <div class="navbar-nav">
-                    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                    <a class="nav-item nav-link" href="./features">Features</a>
-                    <a class="nav-item nav-link" href="./pricing">Pricing</a>
+                    <a class="nav-item nav-link active" href="#">Home</a>
+                    <a class="nav-item nav-link" href="./features">Caratteristiche</a>
+                    <a class="nav-item nav-link" href="./pricing">Prezzo</a>
                   </div>
                 </div>
               </nav>
