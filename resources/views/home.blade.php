015db9c24adfc35a0f8d6de7c4c7a224cbc3676a
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-02-07 17:00:36 +0100

    Added a navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,30 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Laravel Primi Passi</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
     </head>
 
 
 
     <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                  <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="./features">Features</a>
+                    <a class="nav-item nav-link" href="./pricing">Pricing</a>
+                  </div>
+                </div>
+              </nav>
+        </header>
         <main>
             <h1>Hello World</h1>
         </main>
